00756e125e4b0bc0bb35049e829c1945acfb533c
Dhruvi Gandhi <dhruvi9a4@gmail.com>
2026-08-01 09:26:18 +0530

    app.py modified and req.txt added

--- app.py
+++ app.py
@@ -65,6 +65,6 @@ def chat():
     reply = chatbot(user)
 
     return jsonify({"reply": reply})
-
-if __name__=="__main__":
-    app.run(debug=True)
+    
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
